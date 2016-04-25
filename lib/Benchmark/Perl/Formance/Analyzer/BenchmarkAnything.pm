adc497b10b13ce9a6140ece26739740bcab91bf0
Steffen Schwigon <ss5@renormalist.net>
2016-04-25 01:22:16 +0100

    also generate dashboard index.html

--- lib/Benchmark/Perl/Formance/Analyzer/BenchmarkAnything.pm
+++ lib/Benchmark/Perl/Formance/Analyzer/BenchmarkAnything.pm
@@ -23,15 +23,15 @@ has 'name'       => ( is => 'rw', isa => 'ArrayRef', documentation => "file name
 has 'outfile'    => ( is => 'rw', isa => 'Str',      documentation => "output file" );
 has 'verbose'    => ( is => 'rw', isa => 'Bool',     documentation => "Switch on verbosity" );
 has 'debug'      => ( is => 'rw', isa => 'Bool',     documentation => "Switch on debugging output" );
-has 'whitelist'  => ( is => 'rw', isa => 'Str',      documentation => "metricss to use (regular expression)" );
+has 'whitelist'  => ( is => 'rw', isa => 'Str',      documentation => "metrics to use (regular expression)" );
 has 'blacklist'  => ( is => 'rw', isa => 'Str',      documentation => "metrics to skip (regular expression)" );
 has 'dropnull'   => ( is => 'rw', isa => 'Bool',     documentation => "Drop metrics with null values", default => 0 );
 has 'query'      => ( is => 'rw', isa => 'Str',      documentation => "Search query file or '-' for STDIN", default => "-" );
 has 'balib'      => ( is => 'rw',                    documentation => "where to search for benchmark results", default => sub { BenchmarkAnything::Storage::Frontend::Lib->new } );
 has 'template'   => ( is => 'rw', isa => 'Str',
                       documentation => 'output template file',
-                      default => 'google-chart-area.tt',
-                      #default => 'google-chart-line.tt',
+                      default => 'google-chart-area',
+                      #default => 'google-chart-line',
                     );
 has 'tt'         => ( is => 'rw',
                       documentation => "template renderer",
@@ -50,6 +50,8 @@ has 'x_type'      => ( is => 'rw', isa => 'Str',      documentation => "x-axis t
 has 'y_key'       => ( is => 'rw', isa => 'Str',      documentation => "y-axis key",  default => "VALUE" );
 has 'y_type'      => ( is => 'rw', isa => 'Str',      documentation => "y-axis type", default => "numeric" );
 has 'aggregation' => ( is => 'rw', isa => 'Str',      documentation => "which aggregation to use (avg, stdv, ci_95_lower, ci_95_upper)", default => "avg" );     # sub entries of {stats}: avg, stdv, ci_95_lower, ci_95_upper
+has 'querybundle' => ( is => 'rw', isa => 'Str',      documentation => "which chartqueries/ subdirectory (e.g., perlformance, perlstone2015)", default => 'perlstone2015' );
+has 'rawnumbers'  => ( is => 'rw', isa => 'Str',      default => "" );
 
 use namespace::clean -except => 'meta';
 __PACKAGE__->meta->make_immutable;
@@ -74,22 +76,56 @@ sub _print_to_template
         my ($self, $RESULTMATRIX, $options) = @_;
 
         require JSON;
+        my $outfile = $options->{outfile};
+        my $title = $options->{charttitle} || "";
+
+        my $vars = {
+                    RESULTMATRIX     => JSON->new->pretty->encode($RESULTMATRIX),
+                    title            => $title,
+                    outfile          => $outfile,
+                    x_key            => $options->{x_key},
+                    isStacked        => $options->{isStacked},
+                    interpolateNulls => $options->{interpolateNulls},
+                    areaOpacity      => $options->{areaOpacity},
+                    width            => 700,
+                    height           => 500,
+                   };
+
+        my $template = $self->template.".tt";
+        $self->tt->process($template, $vars, ($outfile eq '-' ? () : $outfile))
+         or die $self->tt->error."\n";
+}
+
+sub _print_to_template_multi
+{
+        my ($self, $chartlist, $options) = @_;
+
+        require JSON;
+
+        my $number = 0;
+        my @extended_chartlist = map
+        {
+                $_->{json} = JSON->new->pretty->encode($_->{data});
+                $_->{number} = $number++;
+                $_
+        } @$chartlist;
 
         # print
         my $vars = {
-                    RESULTMATRIX     => JSON->new->pretty->encode( $RESULTMATRIX ),
-                    title            => 'Perl::Formance - '.($options->{charttitle} || ""),
+                    querybundle      => $options->{querybundle},
+                    chartlist        => \@extended_chartlist,
+                    width            => $options->{width} || 300,
+                    height           => $options->{height} || 200,
                     x_key            => $options->{x_key},
                     isStacked        => $options->{isStacked},
                     interpolateNulls => $options->{interpolateNulls},
                     areaOpacity      => $options->{areaOpacity},
                    };
-
-        # to STDOUT
-        my $outfile = $options->{outfile};
-
-        $self->tt->process($self->template, $vars, ($outfile eq '-' ? () : $outfile))
+        my $template_multi = $self->template."_multi.tt";
+        my $resultbuffer;
+        $self->tt->process($template_multi, $vars, \$resultbuffer)
          or die $self->tt->error."\n";
+        return $resultbuffer;
 }
 
 sub _get_chart
@@ -98,7 +134,8 @@ sub _get_chart
 
         require File::Slurper;
 
-        my $filename = dist_dir('Benchmark-Perl-Formance-Analyzer')."/chartqueries/perlformance/$chartname.json";
+        my $querybundle = $self->querybundle;
+        my $filename = dist_dir('Benchmark-Perl-Formance-Analyzer')."/chartqueries/$querybundle/$chartname.json";
         my $json = File::Slurper::read_text($filename);
         if ($self->debug) {
                 say STDERR "READ: $chartname - $filename";
@@ -134,16 +171,22 @@ sub run
         require File::Basename;
         require BenchmarkAnything::Evaluations;
 
-        say STDERR sprintf("Perl::Formance - chart rendering: ".~~gmtime."\n") if $self->verbose;
+        my $timestamp = ~~gmtime;
+        my $headline  = "Perl::Formance - chart rendering: $timestamp\n";
+        say STDERR sprintf($headline) if $self->verbose;
+        $self->rawnumbers($self->rawnumbers.$headline);
+
+        my $querybundle = $self->querybundle;
 
         my @chartnames =
          map { File::Basename::basename($_, ".json") }
           File::Find::Rule
                    ->file
                     ->name( '*.json' )
-                     ->in( dist_dir('Benchmark-Perl-Formance-Analyzer')."/chartqueries/perlformance/" );
+                     ->in( dist_dir('Benchmark-Perl-Formance-Analyzer')."/chartqueries/$querybundle/" );
 
-        foreach my $chartname (@chartnames)
+        my @chartlist;
+        foreach my $chartname (sort @chartnames)
         {
 
                 my $chart             = $self->_get_chart($chartname);
@@ -157,7 +200,8 @@ sub run
                                          verbose     => $self->verbose,
                                          debug       => $self->debug,
                                         };
-                my $result_matrix = BenchmarkAnything::Evaluations::transform_chartlines($chartlines, $transform_options);
+                my ($result_matrix, $rawnumbers) = BenchmarkAnything::Evaluations::transform_chartlines($chartlines, $transform_options);
+                $self->rawnumbers($self->rawnumbers."\n$rawnumbers");
 
                 my $outfile;
                 if (not $outfile  = $self->outfile)
@@ -166,7 +210,7 @@ sub run
                         $outfile  =  $chartname;
                         $outfile  =~ s/[\s\W:]+/-/g;
                         $outfile .=  ".html";
-                        $outfile  = File::HomeDir->my_home . "/perlformance/results/".$outfile;
+                        $outfile  = File::HomeDir->my_home . "/perlformance/results/$querybundle/".$outfile;
                 }
                 my $render_options = {
                                       x_key            => $self->x_key,
@@ -178,9 +222,36 @@ sub run
                                      };
                 $self->_print_to_template($result_matrix, $render_options);
 
+                push @chartlist, {
+                                  outfile    => File::Basename::basename($outfile),
+                                  data       => $result_matrix,
+                                  charttitle => ($chart->{charttitle} || $chartname),
+                                 };
                 say STDERR "Done." if $self->verbose;
 
         }
+
+        # DASHBOARD
+        my $dashboard_options = {
+                                 x_key            => $self->x_key,
+                                 isStacked        => "false", # true, false, 'relative'
+                                 interpolateNulls => "true",  # true, false -- only works with isStacked=false
+                                 areaOpacity      => 0.0,
+                                 querybundle      => $querybundle,
+                                };
+        my $dashboard_file    = File::HomeDir->my_home . "/perlformance/results/$querybundle/index.html";
+        my $dashboard_content = $self->_print_to_template_multi(\@chartlist, $dashboard_options);
+        open my $DASHBOARD, ">", $dashboard_file or die "Could not write to $dashboard_file";
+        print $DASHBOARD $dashboard_content;
+        close $DASHBOARD;
+
+        # RAW NUMBERS
+        my $rawnumbers_file    = File::HomeDir->my_home . "/perlformance/results/$querybundle/raw-numbers.txt";
+        open my $RAWNUMBERS, ">", $rawnumbers_file or die "Could not write to $rawnumbers_file";
+        print $RAWNUMBERS $self->rawnumbers;
+        close $RAWNUMBERS;
+
+        # Done
         return;
 }
 
